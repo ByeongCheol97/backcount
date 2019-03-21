498ef8f7abf05cda0c960ab186f621385330b478
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-21 12:33:19 +0900

    commit

--- backcount/ViewController.swift
+++ backcount/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var count = 9
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(count)
     }
 
-
+    @IBAction func but(_ sender: Any) {
+        if count != 0 {
+            count = count - 1
+            label.text = String(count)
+        } else
+        {
+            count = 9;
+            label.text = String(count)
+        }
+    }
 }
-
